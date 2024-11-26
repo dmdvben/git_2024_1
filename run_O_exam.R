0ae70b973bc52c488161dc6f9b8baa41d266728a
dmdvben <intro_data_science_202x@hotmail.com>
2024-11-26 15:30:52 +0100

    Updating run_O_exam.R

--- run_O_exam.R
+++ run_O_exam.R
@@ -1,4 +1,5 @@
-# Path to the "fun_o_exam.R" file
+# Path to the "fun_O_exam.R" file
+# getwd()
 file_path <- "insert path to fun_o_exam.R file here"
 source(file_path)
 flow_id("insert your flow ID here")
